9e4b965ca31bb22dfed5369fed710c59404dea2c
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-07-04 17:16:18 +0100

    my weather app

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,12 +4,13 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'chelsy237',
-  database: 'journal-entries',
-  port: '5432',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 
 pool
   .connect()
